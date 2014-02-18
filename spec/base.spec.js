1993fe4fcd5dcb4019f45d114d168240f42cb4ea
Lucas Doyle <lukedoyle@mujin.co.jp>
2014-02-18 12:04:56 +0900

    fix base tests

--- spec/base.spec.js
+++ spec/base.spec.js
@@ -26,5 +26,41 @@ describe('base', function() {
 
     // mixed int, string, float
     expect(shrike.sum([0, '1.5', 2.3])).toBe(3.8);
+
+    // empty sums should be 0
+    expect(shrike.sum([])).toBe(0);
+  });
+
+  it('round', function() {
+
+    // throw on garbage input
+    expect(function() {shrike.round(undefined)}).toThrow();
+    expect(function() {shrike.round(false)}).toThrow();
+    expect(function() {shrike.round('lol wtf')}).toThrow();
+    expect(function() {shrike.round([[1, 2, 3], [1, 2, 4]])}).toThrow();
+    expect(function() {shrike.round(['i\m a string'])}).toThrow();
+    expect(function() {shrike.round([undefined])}).toThrow();
+    expect(function() {shrike.round([[undefined]])}).toThrow();
+
+    // ints
+    expect(shrike.round(1111)).toEqual(1111);
+    expect(shrike.round(1111, 2)).toEqual(1111);
+
+    // more than 20 places
+    expect(function() {shrike.round(1111, 21)}).toThrow();
+
+    // floats
+    expect(shrike.round(1.5235, 3)).toBe(1.524);
+    expect(shrike.round(1.5235, 2)).toBe(1.52);
+    expect(shrike.round(1.5235, 1)).toBe(1.5);
+    expect(shrike.round(1.5235, 0)).toBe(2);
+    expect(shrike.round(1.50000000001, 4)).toBe(1.5);
+
+    // strings
+    expect(shrike.round('1.5235', 3)).toBe(1.524);
+    expect(shrike.round('1.5235', 2)).toBe(1.52);
+    expect(shrike.round('1.5235', 1)).toBe(1.5);
+    expect(shrike.round('1.5235', 0)).toBe(2);
+    expect(shrike.round('1.50000000001', 4)).toBe(1.5);
   });
 });
--- src/base.js
+++ src/base.js
@@ -1,8 +1,9 @@
 define([
 
-  'underscore'
+  'underscore',
+  'mjs'
 
-], function(_) {
+], function(_, mjs) {
   'use strict';
 
   return function(shrike) {
@@ -10,6 +11,11 @@ define([
     // borrow all of Math's functions and constants... except round since it is defined below
     _.without(Object.getOwnPropertyNames(Math), 'round').forEach(function(prop) {
       shrike.register(prop, Math[prop]);
+    });
+
+    // borrow mjs too
+    Object.getOwnPropertyNames(mjs).forEach(function(prop) {
+      shrike.register(prop, mjs[prop]);
     });
 
     // sum an array
@@ -33,13 +39,40 @@ define([
       return parseFloat(x) * parseFloat(x);
     });
 
-    shrike.register('round', function(num, dec) {
+    shrike.register('xround', function(num, dec) {
       dec = dec || 0.0;
       // works to 20 decimal points
 
       return shrike.scalarIterator(num, function(a) {
         return parseFloat(new Number(a + '').toFixed(parseInt(dec)));
       });
+    });
+
+    shrike.register('round', function(n, dec) {
+      dec = dec || 0;
+
+      // works to 20 decimal points
+      if (dec >= 20) {
+        shrike.throwError('round: can only round to 20 decimal places');
+      }
+
+      if (shrike.isNumber(n)) {
+        return parseFloat(new Number(n + '').toFixed(parseInt(dec)));
+      }
+      else {
+        shrike.throwError('round: ' + n + ' is not a numeric type');
+      }
+    });
+
+    shrike.register('roundArray', function(A, dec) {
+      if (shrike.isArray(A)) {
+        return shrike.scalarIterator(A, function(a) {
+          return shrike.round(a, dec);
+        });
+      }
+      else {
+        shrike.throwError('roundArray: not an array');
+      }
     });
   }
 });
